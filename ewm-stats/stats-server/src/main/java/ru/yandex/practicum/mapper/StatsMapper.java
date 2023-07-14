e948eff9a369cbe41bc4bb70042d40800fd63986
NikitaLevshin <levshinno@yandex.ru>
2023-07-15 02:40:53 +0300

    pom fix

--- ewm-stats/stats-server/src/main/java/ru/yandex/practicum/mapper/StatsMapper.java
+++ ewm-stats/stats-server/src/main/java/ru/yandex/practicum/mapper/StatsMapper.java
@@ -5,15 +5,23 @@ import ru.yandex.practicum.model.Stats;
 import ru.yandex.practicum.RequestDto;
 import ru.yandex.practicum.ResponseDto;
 
+import java.time.LocalDateTime;
+
 @UtilityClass
 public class StatsMapper {
 
     public static Stats fromStatsDto(RequestDto requestDto) {
+        LocalDateTime localDateTime;
+        if (requestDto.getCreationDate() == null) {
+            localDateTime = LocalDateTime.now();
+        } else {
+            localDateTime = requestDto.getCreationDate();
+        }
         return Stats.builder()
                 .app(requestDto.getApp())
                 .uri(requestDto.getUri())
                 .ip(requestDto.getIp())
-                .creationDate(requestDto.getCreationDate())
+                .creationDate(localDateTime)
                 .build();
     }
 
